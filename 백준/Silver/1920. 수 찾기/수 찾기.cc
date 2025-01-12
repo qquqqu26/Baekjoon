ba6541961600f37094dd4f45b134b212fe4f48df
qquqqu26 <139879153+qquqqu26@users.noreply.github.com>
2025-01-12 16:11:44 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 2508 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+const int maxSize = 100001;
+int n, m;
+int a[maxSize];
+bool answer[maxSize] = { 0 };
+
+int main() {
+
+	cin.tie(0);
+	ios::sync_with_stdio(0);
+
+	//입력
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+	}
+
+	sort(a+1, a+n+1);
+
+	cin >> m;
+	for (int i = 1; i <= m; i++) {
+		int input;
+		cin >> input;
+
+		//이진 탐색
+		int start = 1;
+		int end = n;
+		int mid = (start + end) / 2;
+
+		while (start <= end) {
+			if (input > a[mid]) {
+				start = mid + 1;
+				mid = (start + end) / 2;
+			}
+			else if (input < a[mid]) {
+				end = mid - 1;
+				mid = (start + end) / 2;
+
+			}
+			else {
+				answer[i] = 1;
+				break;
+			}
+		}
+	}
+
+	//출력
+	for (int i = 1; i <= m; i++) {
+		cout << answer[i] << "\n";
+	}
+
+}
